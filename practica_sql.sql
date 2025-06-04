7a4239b53ab41e44857d23ad1735110869f62077
Micaela Agüero <104102664+micaela-Aguero@users.noreply.github.com>
2025-06-04 16:46:20 -0300

    agrego ejercicio 19

--- practica_sql.sql
+++ practica_sql.sql
@@ -437,3 +437,43 @@ FROM Rubro r JOIN Producto p on r.rubr_id=p.prod_rubro
              JOIN Item_Factura i on i.item_producto=p.prod_codigo   
 GROUP BY r.rubr_detalle, rubr_id
 ORDER BY COUNT(DISTINCT i.item_producto)
+
+/*19. En virtud de una recategorizacion de productos referida a la familia de los mismos se
+solicita que desarrolle una consulta sql que retorne para todos los productos:
+ Codigo de producto
+ Detalle del producto
+ Codigo de la familia del producto
+ Detalle de la familia actual del producto
+ Codigo de la familia sugerido para el producto
+ Detalla de la familia sugerido para el producto
+La familia sugerida para un producto es la que poseen la mayoria de los productos cuyo
+detalle coinciden en los primeros 5 caracteres.
+En caso que 2 o mas familias pudieran ser sugeridas se debera seleccionar la de menor
+codigo. Solo se deben mostrar los productos para los cuales la familia actual sea
+diferente a la sugerida
+Los resultados deben ser ordenados por detalle de producto de manera ascendente*/
+
+SELECT
+p.prod_codigo,
+p.prod_detalle,
+p.prod_familia,
+f.fami_detalle FAMILIA_ACTUAL,
+(SELECT TOP 1 p1.prod_familia
+FROM Producto p1
+WHERE LEFT(p.prod_detalle,5)=LEFT(p1.prod_detalle,5)
+GROUP BY p1.prod_familia
+ORDER BY COUNT(*) DESC, p1.prod_familia) FAMILIA_SUGERIDA,
+(SELECT f3.fami_detalle 
+FROM Familia f3
+WHERE f3.fami_id=(SELECT TOP 1 p1.prod_familia
+                  FROM Producto p1
+                  WHERE LEFT(p.prod_detalle,5)=LEFT(p1.prod_detalle,5)
+                  GROUP BY p1.prod_familia
+                  ORDER BY COUNT(*) DESC,p1.prod_familia)) DETALLE_FAMILIA_SUGERIDA
+FROM Producto p JOIN Familia f on p.prod_familia=f.fami_id
+WHERE p.prod_familia != (SELECT TOP 1 p1.prod_familia
+                        FROM Producto p1
+                        WHERE LEFT(p.prod_detalle,5)=LEFT(p1.prod_detalle,5)
+                        GROUP BY p1.prod_familia
+                        ORDER BY COUNT(*) DESC, p1.prod_familia)
+ORDER BY p.prod_detalle 

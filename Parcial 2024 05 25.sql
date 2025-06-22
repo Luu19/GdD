1af9cf156c1b5b4aa634ad5be7745217c77be2a5
lidanez <lidanez@frba.utn.edu.ar>
2025-06-22 11:58:26 -0300

    mas parciales

---
+++ Parcial 2024 05 25.sql
@@ -0,0 +1,14 @@
+-- Parcial 2024 05 25
+/*
+	Dada la crisis que atraviesa la empresa, el directorio solicita un informe especial para poder
+	analizar y definir la nueva estrategia a adoptar.
+	Este informe consta de un listado de aquellos productos cuyas ventas de lo que va del año 2012
+	fueron superiores al 15% del promedio de ventas de los productos vendidos entre los años 2010 y 2011.
+	En base a lo solicitado, armar una consulta SQL que retorne la siguiente informacion:
+		1. Detalle del producto
+		2. Mostrar la leyenda "Popular" si dicho producto figura en más de 100 facturas realizadas
+		en el 2012. Caso contrario, mostrar una leyenda "Sin interes"
+		3. Cantidad de facturas en las que aparece el producto en el año 2012
+		4. Codigo de cliente que mas compro dicho producto en el 2012 (en caso de existir más de un cliente, mostrar
+		solamente el de menor codigo)
+*/
